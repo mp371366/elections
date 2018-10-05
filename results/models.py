927941c952a2030d123a19a1398fce308401365c
mateusz <mateusz@localhost.localdomain>
2018-10-05 21:02:15 +0200

    Add models

--- results/models.py
+++ results/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Candidate(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "{self.name} {self.surname}"
+
+class Voivodeship(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "Województwo {self.name}"
+
+class District(models.Model):
+    name = models.CharField(max_length=40)
+    voivodeship = models.ForeignKey(Voivodeship, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Powiat {self.name}"
+
+class Unit(models.Model):
+    name = models.CharField(max_length=40)
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Gmina {self.name}"
+
+class Result(models.Model):
+    result = models.PositiveIntegerField()
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{self.result}"
